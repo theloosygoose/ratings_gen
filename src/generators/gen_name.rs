ff6d172777d9230645473c47104104083df11d16
Gusti Henry <g.ramahenry2439@gmail.com>
2023-04-17 16:23:35 -0400

    generate ethnicity

--- src/generators/gen_name.rs
+++ src/generators/gen_name.rs
@@ -1,8 +1,14 @@
-use std::{error::Error, io, process};
-use csv;
+use std::error::Error;
+use std::fs;
+use csv::Reader;
 
 pub fn gen_name() -> Result<(), Box<dyn Error>> {
-    let mut rdr = csv::Reader::from_path("../../data/nationalities.csv")?;
+    
+    let csv_string:String = fs::read_to_string("../../data/nationality.txt")?.parse()?;
+
+    println!("{:#?}", csv_string);
+    
+    let mut rdr = Reader::from_reader(csv_string.as_bytes());
 
     for result in rdr.records() {
         let record = result?;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use crate::player::Player;
+// use crate::player::Player;
+use std::fs;
 use crate::generators::gen_name;
+use std::error::Error;
 // use crate::generators::gen_ratings::print_many_ratings;
 // use crate::generators::constants::{MEAN_RTG, MEAN_STD_DEV, WIDE_STD_DEV};
 
 pub mod ratings;
 pub mod generators;
 pub mod player;
+
+fn readfile() -> String {
+    let string = match fs::read_to_string("../data/nationality.txt") {
+        Ok(yes) => yes,
+        Err(err) => err.to_string() 
+    };
+    
+    return string;
+}
 
 fn main() {
     // let mut players = vec![];
@@ -18,11 +29,14 @@ fn main() {
     //     n += 1;
     // }
     // println!("Players: {:#?}", players);
+    // 
+    let csv_string = readfile();
+
+    println!("{:#?}", csv_string);
 
     match gen_name::gen_name() {
         Ok(res) => println!("Read Csv {:?}", res),
         Err(err) => println!("Error Reading CSV {:#?}", err),
-        
     } 
 
 }
